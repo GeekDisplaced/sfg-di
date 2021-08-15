db236b2c3378e7142f98388ff12d3fbe5a54b223
GeekDisplaced <geek.displaced@icloud.com>
2021-08-15 15:14:08 +0100

    Added manual DI examples

---
+++ src/test/java/guru/springframework/sfgdi/controller/PropertyInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgdi.controller;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setup() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
