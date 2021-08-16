8ec7feecc0ad0d666e47525ed11cb5ed6fc7c331
GeekDisplaced <geek.displaced@icloud.com>
2021-08-16 09:44:59 +0100

    Added @Primary annotation DI examples

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -15,27 +15,25 @@ public class SfgDiApplication {
 
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-		MyController myController = (MyController) ctx.getBean("myController");
-
-		String greeting = myController.sayHello();
-
-		System.out.println(greeting);
-
-		System.out.println("----------   Property Injection   ---------");
+		System.out.println("----------   Property Injection & Qualifier Annotation   ---------");
 
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 
-		System.out.println("----------    Setter Injection    ---------");
+		System.out.println("----------    Setter Injection & Qualifier Annotation    ---------");
 
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
 		System.out.println(setterInjectedController.getGreeting());
 
-		System.out.println("---------- Constructor  Injection ---------");
+		System.out.println("---------- Constructor  Injection & Qualifier Annotation ---------");
 
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
 
+		System.out.println("---------- Constructor Injection & Primary Annotation ---------");
+
+		MyController myController = (MyController) ctx.getBean("myController");
+		System.out.println(myController.getGreeting());
 	}
 
 }
--- src/main/java/guru/springframework/sfgdi/controller/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controller/MyController.java
@@ -1,14 +1,19 @@
 package guru.springframework.sfgdi.controller;
 
+import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-    public String sayHello(){
+    private final GreetingService greetingService;
 
-        System.out.println("Hello World!!!");
-
-        return "Hi Folks!";
+    // no @Qualifier here so will use the @Primary
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    };
 }
