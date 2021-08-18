f1537933b0d43e9170b147b8fed16c0ca1ca459a
GeekDisplaced <geek.displaced@icloud.com>
2021-08-18 13:27:12 +0100

    Refactor to use Java Config rather than @Primary @Profile

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,32 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.InjectedByConstructorGreetingService;
-import guru.springframework.sfgdi.services.InjectedByPropertyGreetingService;
-import guru.springframework.sfgdi.services.InjectedBySetterGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile("en")
+    @Primary
+    @Bean("i18nGreetingService")
+    I18nEnglishGreetingService i18nEnglishGreetingService() {
+
+    }
+
+    @Profile("de")
+    @Bean("i18nGreetingService")
+    I18nGermanGreetingService i18nGermanGreetingService() {
+
+    }
+
+    @Profile("es")
+    @Bean("i18nGreetingService")
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+
+    }
 
     @Bean
     InjectedByConstructorGreetingService injectedByConstructorGreetingService() {
--- src/main/java/guru/springframework/sfgdi/services/I18nGermanGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nGermanGreetingService.java
@@ -1,10 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Profile("de")
-@Service("I18nGreetingService")
 public class I18nGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
